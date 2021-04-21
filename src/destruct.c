6906c19f000a9075aa9b403abe452e9820e9cf1a
Sergii Pylypenko <x.pelya.x@gmail.com>
2021-04-22 01:50:55 +0300

    Fixed two player mode keys in Destruct

--- src/destruct.c
+++ src/destruct.c
@@ -350,23 +350,23 @@ static const JE_byte ModeScore[MAX_PLAYERS][MAX_MODES] =
 
 static SDL_Scancode defaultKeyConfig[MAX_PLAYERS][MAX_KEY][MAX_KEY_OPTIONS] =
 {
-	{	{SDL_SCANCODE_C},
-		{SDL_SCANCODE_V},
-		{SDL_SCANCODE_A},
-		{SDL_SCANCODE_Z},
-		{SDL_SCANCODE_LALT},
-		{SDL_SCANCODE_X, SDL_SCANCODE_LSHIFT},
-		{SDL_SCANCODE_LCTRL},
-		{SDL_SCANCODE_SPACE}
+	{	{SDL_SCANCODE_LEFT},
+		{SDL_SCANCODE_RIGHT},
+		{SDL_SCANCODE_UP},
+		{SDL_SCANCODE_DOWN},
+		{SDL_SCANCODE_KP_ENTER},
+		{SDL_SCANCODE_RETURN},
+		{SDL_SCANCODE_AC_BACK},
+		{SDL_SCANCODE_AC_FORWARD}
 	},
-	{	{SDL_SCANCODE_LEFT, SDL_SCANCODE_KP_4, SDL_SCANCODE_2},
-		{SDL_SCANCODE_RIGHT, SDL_SCANCODE_KP_6, SDL_SCANCODE_8},
-		{SDL_SCANCODE_UP, SDL_SCANCODE_KP_8, SDL_SCANCODE_6},
-		{SDL_SCANCODE_DOWN, SDL_SCANCODE_KP_2, SDL_SCANCODE_4},
-		{SDL_SCANCODE_BACKSLASH, SDL_SCANCODE_KP_5, SDL_SCANCODE_5},
-		{SDL_SCANCODE_INSERT, SDL_SCANCODE_RETURN, SDL_SCANCODE_KP_0, SDL_SCANCODE_KP_HASH},
-		{SDL_SCANCODE_PAGEUP, SDL_SCANCODE_KP_9, SDL_SCANCODE_0},
-		{SDL_SCANCODE_PAGEDOWN, SDL_SCANCODE_KP_3, SDL_SCANCODE_KP_MULTIPLY}
+	{	{SDL_SCANCODE_2},
+		{SDL_SCANCODE_8},
+		{SDL_SCANCODE_6},
+		{SDL_SCANCODE_4},
+		{SDL_SCANCODE_KP_HASH},
+		{SDL_SCANCODE_5},
+		{SDL_SCANCODE_0},
+		{SDL_SCANCODE_KP_MULTIPLY}
 	}
 };
 
@@ -614,8 +614,14 @@ static void JE_introScreen( void )
 {
 	memcpy(VGAScreen2->pixels, VGAScreen->pixels, VGAScreen2->h * VGAScreen2->pitch);
 	JE_outText(VGAScreen, JE_fontCenter(specialName[7], TINY_FONT), 90, specialName[7], 12, 5);
-	JE_outText(VGAScreen, JE_fontCenter(miscText[64], TINY_FONT), 180, miscText[64], 15, 2);
-	JE_outText(VGAScreen, JE_fontCenter(miscText[65], TINY_FONT), 190, miscText[65], 15, 2);
+	//JE_outText(VGAScreen, JE_fontCenter(miscText[64], TINY_FONT), 180, miscText[64], 15, 2);
+	//JE_outText(VGAScreen, JE_fontCenter(miscText[65], TINY_FONT), 190, miscText[65], 15, 2);
+	const char *Help1 = "Press Call key for two player mode";
+	const char *Help2 = "Player 1 keys: 2 4 8 6 5 0 # *";
+	const char *Help3 = "Player 2 keys: D-pad Call SoftLeft SoftRight";
+	JE_outText(VGAScreen, JE_fontCenter(Help1, TINY_FONT), 110, Help1, 15, 2);
+	JE_outText(VGAScreen, JE_fontCenter(Help2, TINY_FONT), 120, Help2, 15, 2);
+	JE_outText(VGAScreen, JE_fontCenter(Help3, TINY_FONT), 130, Help3, 15, 2);
 	JE_showVGA();
 	fade_palette(colors, 15, 0, 255);
 
@@ -1479,6 +1485,13 @@ static enum de_state_t DE_RunTick( void )
 	{
 		destruct_player[PLAYER_RIGHT].is_cpu = !destruct_player[PLAYER_RIGHT].is_cpu;
 		keysactive[SDL_SCANCODE_F11] = false;
+	}
+	if (!destruct_player[PLAYER_LEFT].is_cpu
+		&& (keysactive[SDL_SCANCODE_KP_ENTER]
+		|| keysactive[SDL_SCANCODE_AC_BACK]
+		|| keysactive[SDL_SCANCODE_AC_FORWARD]))
+	{
+		destruct_player[PLAYER_LEFT].is_cpu = false;
 	}
 	if (keysactive[SDL_SCANCODE_P])
 	{
